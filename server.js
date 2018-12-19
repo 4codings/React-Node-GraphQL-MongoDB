e2b28ebf6b7718d0ea44a27b013c88d291ce7cfe
4codings <proudofdeveloper@gmail.com>
2018-12-19 23:14:20 +0100

    configure and connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema.js');
 
@@ -10,6 +11,15 @@ app.use('/graphql',
         graphiql: true
     })
 );
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected ..'))
+    .catch(err => console.log(err));
 
 const PORT = process.env.PORT | 4000
 
