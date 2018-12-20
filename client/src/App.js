bf4017320b9a80a9074567f917e9e6009a1d951e
4codings <proudofdeveloper@gmail.com>
2018-12-20 14:48:20 +0100

    set-up webpack and view files

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (
-            <div className="App" />
+            <div className="App">
+                <h1>Hello World</h1>
+            </div>
         );
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello World</h1>
-            </div>
-        );
-    }
-}
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
