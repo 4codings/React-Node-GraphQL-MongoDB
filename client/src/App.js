62caa70a84aa11fbb7d03382841eebda36ef63ad
4codings <proudofdeveloper@gmail.com>
2018-12-20 16:01:45 +0100

    connect api to client, fetch data with queries

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import Customers from './components/Customers';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+});
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Hello World</h1>
-            </div>
+            <ApolloProvider client={client}>
+                <div className="App">
+                    <Customers />
+                </div>
+            </ApolloProvider>
         );
     }
 }
---
+++ client/src/queries.js
@@ -0,0 +1,21 @@
+import gql from 'graphql-tag';
+
+export const GET_CUSTOMERS = gql`
+    {
+        customers {
+            id
+            name
+            email
+        }
+    }
+`;
+
+export const GET_CUSTOMER = gql`
+    {
+        customer($id: String!) {
+            id
+            name
+            email
+        }
+    }
+`;
