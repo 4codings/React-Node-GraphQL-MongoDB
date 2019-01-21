6cfca0721a5582f304093a7a7affd43fd6bd238b
4codings <proudofdeveloper@gmail.com>
2019-01-21 23:31:27 +0100

    set-up routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react';
+import '@babel/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import queryString from 'query-string';
+import history from './history';
 import Customers from './components/Customers';
+import router from './router';
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
 });
+const container = document.getElementById('root');
 
-class App extends Component {
-    render() {
-        return (
+const render = async (location) => {
+    try {
+        const context = {
+            pathname: location.pathname,
+            query: queryString.parse(location.search),
+        };
+        const route = await router.resolve(context);
+
+        if (route.redirect) {
+            history.replace(route.redirect);
+            return;
+        }
+
+        ReactDOM.render(
             <ApolloProvider client={client}>
                 <div className="App">
                     <Customers />
+                    {route.component}
                 </div>
-            </ApolloProvider>
+            </ApolloProvider>,
+            container,
+            () => document.title = route.title,
         );
+    } catch (error) {
+        console.error(error);
     }
-}
+};
 
-export default App;
+history.listen(render);
+render(history.location);
---
+++ client/src/routes/index.js
@@ -0,0 +1,17 @@
+const routes = {
+    path: '/',
+    async action({ next }) {
+        const route = await next();
+        route.title = `${route.title || 'Untitled Page'}`;
+        route.description = route.description || '';
+        return route;
+    },
+    children: [
+        {
+            path: '/',
+            load: () => import(/* webpackChunkName: 'home' */ './login'),
+        },
+    ],
+};
+
+export default routes;
