353270999029338b9e94be21a09fa9256627fb06
4codings <proudofdeveloper@gmail.com>
2019-01-24 20:08:16 +0100

    fix some typescript errors

--- client/src/components/link/Link.tsx
+++ client/src/components/link/Link.tsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import history from '../../history';
 import * as s from './Link.css';
 
-class Link extends Component {
-    static propTypes = {
-        to: PropTypes.string.isRequired,
-        children: PropTypes.node.isRequired,
-    };
+export interface LinkProps {
+  to: string;
+  children: string;
+}
 
+class Link extends Component<LinkProps> {
     handleClick = (e) => {
         e.preventDefault();
         history.push({
